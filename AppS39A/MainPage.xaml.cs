ac039bd7b59438d946ed66d1ca999ae9ace5cbe7
Andrea <Andrea@ANDREA>
2021-10-19 19:30:23 -0500

    Agregar archivos de proyecto.

---
+++ AppS39A/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS39A
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string usuario = txtUsuario.Text;
+                string clave = txtClave.Text;
+
+                //Permite abrir la ventana (viewDos)
+                await Navigation.PushAsync(new ViewDos(usuario, clave));
+            }
+
+            catch (Exception ex)
+            {
+                await DisplayAlert("Mensaje de advertencia", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ AppS39A/ViewDos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS39A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewDos : ContentPage
+    {
+       
+        public ViewDos(string usuario, string clave)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+            lblClave.Text = clave;
+            txtUsuario.Text = usuario;
+            txtClave.Text = clave;
+
+        }
+    }
+}
